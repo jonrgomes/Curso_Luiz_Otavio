506ede21edfebf00f028cad636826fb19e8f2367
jonrgomes <jonrgomes@gmail.com>
2026-07-15 20:07:20 -0300

    Curso_LOM

---
+++ Aula_012_LOM.py
@@ -0,0 +1,12 @@
+nome = 'Jonatas Gomes'
+altura = 1.69
+peso = 70
+imc = peso / (altura * altura)
+print(f'O IMC de {nome} é {imc:.2f}!')
+
+
+# Jonatas Gomes tem 1.69 de altura, 
+# pesa 70 quilos e seu IMC é 
+# 29.320987654320987
+# formula = IMC = peso / (altura * altura)
+
