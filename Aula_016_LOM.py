3c899afb5f81c5815c88c22e64d679542ff94de8
jonrgomes <jonrgomes@gmail.com>
2026-07-19 13:07:48 -0300

    Curso_LOM

---
+++ Aula_016_LOM.py
@@ -0,0 +1,11 @@
+# if / elif     / else
+# se / se não se / se não
+entrada = input('Você quer entrar ou sair? ')
+
+if entrada == 'entrar':
+    print('Você entrou do sistema')
+elif entrada == 'sair': 
+    print('Você saiu do sitema')
+else: 
+    print('Você não digitou nem entrar nem sair!')
+    
