ac42903396559ba8b209809be6f85df396fec8a5
jonrgomes <jonrgomes@gmail.com>
2026-07-19 12:53:51 -0300

    Curso_LOM

---
+++ Aula_015_LOM.py
@@ -0,0 +1,15 @@
+# nome = input('Qual o seu nome? ')
+# numero_1 = int(input('Digite um número: '))
+# numero_2 = int(input('Digite outro número: '))
+# soma = numero_1 + numero_2
+# print(f'Seu nome é {nome}!')
+# print(f'A soma dos números é {soma}')
+
+# Tipagem de número
+numero_01 = input('Digite um número: ')
+numero_02 = input('Digite outro número: ')
+
+int_numero_01 = int(numero_01)
+int_numero_02 = int(numero_02)
+
+print(f'A soma dos números e: {int_numero_01 + int_numero_02}')
